55570cccc8df0292f0b51fdd524f4e049f06f81f
Moises Alejandro <10478252+MoisesAlejandro@users.noreply.github.com>
2026-09-07 17:47:06 -0400

    Add GitHub Gist sync for known words

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = 'hskwall-v1';
+const C = 'hskwall-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(C).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
