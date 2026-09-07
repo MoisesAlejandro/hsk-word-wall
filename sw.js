ebb2910b3f14177a26345c6584c0ea3a695fbfc7
Moises Alejandro <10478252+MoisesAlejandro@users.noreply.github.com>
2026-09-07 18:08:17 -0400

    v1.1.0: fix wrong definitions/readings, add version number

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const C = 'hskwall-v2';
+// HSK word wall v1.1.0
+const C = 'hskwall-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(C).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
