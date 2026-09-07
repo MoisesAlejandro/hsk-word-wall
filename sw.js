e4df9209efa03b567082cf86dd560a8b291e3cad
Moises Alejandro <10478252+MoisesAlejandro@users.noreply.github.com>
2026-09-07 19:47:47 -0400

    v1.3.0: handwriting practice with stroke order (Hanzi Writer)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// HSK word wall v1.2.1
-const C = 'hskwall-v5';
-const ASSETS = ['./', './index.html', './manifest.json', './class-data.js', './icon-192.png', './icon-512.png'];
+// HSK word wall v1.3.0
+const C = 'hskwall-v6';
+const ASSETS = ['./', './index.html', './manifest.json', './class-data.js', './hanzi-writer.min.js', './strokes-data.js', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(C).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
